705ff8369d3b3b7c2735637c91a1f7aed50b1cf5
Paul Brookes <pbrookes@pang.phys.ucl.ac.uk>
2017-02-02 21:35:00 +0000

    Created derivative spec_multi_2.derivative function.

--- spectroscopy/spec_multi_2.py
+++ spectroscopy/spec_multi_2.py
@@ -31,7 +31,42 @@ class CurvatureInfo:
         self.wd_points = wd_points
         self.transmissions = transmissions
 
+def derivative(x, y, n_derivative = 1):
+    
+    derivatives = np.zeros(y.size - 1)
+    positions = np.zeros(x.size - 1)
+    for index in np.arange(y.size - 1):
+        grad = (y[index + 1] - y[index]) / (x[index + 1] - x[index])
+        position = np.mean([x[index], x[index + 1]])
+        derivatives[index] = grad
+        positions[index] = position
 
+    if n_derivative > 1:
+        positions, derivatives = derivative(positions, derivatives, n_derivative - 1)
+
+    return positions, derivatives
+
+def curvature_vector(wd_points, transmissions):
+
+    is_ordered = all([wd_points[i] <= wd_points[i + 1] for i in xrange(len(wd_points) - 1)])
+    assert is_ordered, "Vector of wd_points is not ordered."
+    assert len(wd_points) == len(transmissions), "Vectors of wd_points and transmissions are not of equal length."
+
+    metric_vector = []
+    for index in range(len(wd_points) - 2):
+        metric = curvature(wd_points[index:index + 3], transmissions[index:index + 3])
+        metric_vector.append(metric)
+    return metric_vector
+
+def curvature(wd_triplet, transmissions_triplet):
+
+    wd_delta_0 = wd_triplet[1] - wd_triplet[0]
+    wd_delta_1 = wd_triplet[2] - wd_triplet[1]
+    transmissions_delta_0 = transmissions_triplet[1] - transmissions_triplet[0]
+    transmissions_delta_1 = transmissions_triplet[2] - transmissions_triplet[1]
+    metric = 2 * (wd_delta_1 * transmissions_delta_1 - wd_delta_0 * transmissions_delta_0) / (wd_delta_0 + wd_delta_1)
+    abs_normalised_metric = np.absolute(metric / transmissions_triplet[1])
+    return abs_normalised_metric
 
 def hamiltonian(params, wd):
     a = tensor(destroy(params.c_levels), qeye(params.t_levels))
@@ -81,44 +116,6 @@ def generate_points(wd_points, indices):
     unique_set = set(new_wd_points) - set(wd_points)
     new_wd_points_unique = np.array(list(unique_set))
     return new_wd_points_unique
-
-def curvature_vector(wd_points, transmissions):
-
-    is_ordered = all([wd_points[i] <= wd_points[i + 1] for i in xrange(len(wd_points) - 1)])
-    assert is_ordered, "Vector of wd_points is not ordered."
-    assert len(wd_points) == len(transmissions), "Vectors of wd_points and transmissions are not of equal length."
-
-    metric_vector = []
-    for index in range(len(wd_points) - 2):
-        metric = curvature(wd_points[index:index + 3], transmissions[index:index + 3])
-        metric_vector.append(metric)
-    return metric_vector
-
-def curvature(wd_triplet, transmissions_triplet):
-
-    wd_are_floats = all([isinstance(wd_triplet[i], float) for i in xrange(len(wd_triplet) - 1)])
-    assert wd_are_floats, "The vector wd_triplet contains numbers which are not floats."
-    transmissions_are_floats = all([isinstance(transmissions_triplet[i], float) \
-                                    for i in xrange(len(transmissions_triplet) - 1)])
-    assert transmissions_are_floats, "The vector transmissions_triplet contains numbers which are not floats."
-
-
-    wd_delta_0 = wd_triplet[1] - wd_triplet[0]
-    wd_delta_1 = wd_triplet[2] - wd_triplet[1]
-    transmissions_delta_0 = transmissions_triplet[1] - transmissions_triplet[0]
-    transmissions_delta_1 = transmissions_triplet[2] - transmissions_triplet[1]
-    metric = 2 * (wd_delta_1 * transmissions_delta_1 - wd_delta_0 * transmissions_delta_0) / (wd_delta_0 + wd_delta_1)
-    abs_normalised_metric = np.absolute(metric / transmissions_triplet[1])
-    return abs_normalised_metric
-
-def y_lim_calc(y_points):
-    buffer_fraction = 0.1
-    y_max = np.amax(y_points)
-    y_min = np.amin(y_points)
-    range = y_max - y_min
-    y_lim_u = y_max + buffer_fraction * range
-    y_lim_l = y_min - buffer_fraction * range
-    return np.array([y_lim_l, y_lim_u])
 
 def sweep(eps, wd_lower, wd_upper, params, fidelity):
     params.eps = eps
